88dd7fde4a51a683c57c1bdf7d03101f59d6d07b
Paola Rodriguez <240062@utxicotepec.edu.mx>
2026-03-17 11:52:03 -0600

    Se añadieron los documentos

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyPaser = require('body-parser');
+
+const http = require('http');
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyPaser.json());
+app.use(bodyPaser.urlencoded({extended: false}));
+
+app.get('/',(req,res)=>res.status(200).send({
+    menssage:'Bienvenido a mi API de tienda virtual',
+}));
+
+const port= parseInt(process.env.PORT,10)|| 8000;
+app.set('port',port);
+const server = http.createServer(app);
+server.listen(port);
+module.exports=app;
